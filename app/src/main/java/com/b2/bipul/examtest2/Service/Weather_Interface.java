5dda42626c9c4e180c75eb9cec32b6dfd896bdfc
sarkarbipul <44293501+sarkarbipul@users.noreply.github.com>
2020-03-10 12:49:44 +0600

    API_KEY

--- app/src/main/java/com/b2/bipul/examtest2/Service/Weather_Interface.java
+++ app/src/main/java/com/b2/bipul/examtest2/Service/Weather_Interface.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface Weather_Interface {
-    @GET("current?access_key=d1db9ab7631ab1d629908236c3b9d55a")
+    @GET("current?access_key=MY_API_KEY")
     Call<WeatherData> listRepos(@Query("query") String key);
 }
